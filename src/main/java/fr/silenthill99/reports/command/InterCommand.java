dc7976dbb1b356c21fc0a4cdace8025f42e41881
BuildTools <unconfigured@null.spigotmc.org>
2024-07-01 22:51:47 +0200

    Correction de la commande d'interadmin

--- src/main/java/fr/silenthill99/reports/command/InterCommand.java
+++ src/main/java/fr/silenthill99/reports/command/InterCommand.java
@@ -1,35 +1,17 @@
 package fr.silenthill99.reports.command;
 
 import fr.silenthill99.reports.Main;
-import fr.silenthill99.reports.inventory.InventoryManager;
-import fr.silenthill99.reports.inventory.InventoryType;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class InterCommand implements CommandExecutor {
 
-    private final Main main;
-    private final Connection conn;
-
-    public InterCommand() {
-        this.main = Main.getInstance();
-        try {
-            this.conn = main.getManager().getDbConnection().getConnection();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private final Main main = Main.getInstance();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String s,
@@ -48,13 +30,11 @@ public class InterCommand implements CommandExecutor {
         Bukkit.getScheduler().runTaskAsynchronously(main, () -> {
             if (!main.getReportManager().isExist(id)) {
                 player.sendMessage(ChatColor.RED + "L'interadmin n°" + id + " n'existe pas.");
+                return;
             }
-        });
-
-        if(args.length == 2 && args[1].equalsIgnoreCase("close")) {
-            Player plaignant = main.getReportManager().getPlaignant(id);
-            Player accuse = main.getReportManager().getAccuse(id);
-            Bukkit.getScheduler().runTaskAsynchronously(main, () -> {
+            if(args.length == 2 && args[1].equalsIgnoreCase("close")) {
+                Player plaignant = main.getReportManager().getPlaignant(id);
+                Player accuse = main.getReportManager().getAccuse(id);
                 main.getReportManager().removeValue(player);
                 Bukkit.getScheduler().runTask(main, () -> {
                     plaignant.teleport(main.getLocManager().getTeleport(plaignant));
@@ -64,14 +44,12 @@ public class InterCommand implements CommandExecutor {
                         main.getLocManager().removeValue(accuse);
                     });
                 });
-            });
-            return true;
-        } else if (args.length == 2) {
-            player.sendMessage(ChatColor.RED + args[1] + " n'est pas un argument valable !");
-        }
-
-        Bukkit.getScheduler().runTaskAsynchronously(main, () -> main.getReportManager().openInter(player, id, args));
-
+            } else if (args.length == 2) {
+                player.sendMessage(ChatColor.RED + args[1] + " n'est pas un argument valable !");
+                return;
+            }
+            main.getReportManager().openInter(player, id, args);
+        });
         return true;
     }
 }
